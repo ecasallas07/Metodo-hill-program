601d6b3d627bb791f63227e24e09c76218d7ea97
ecasallas07 <ecasallas07@gmail.com>
2024-07-02 18:07:36 -0500

    feat:develop complete logic for encrypt message - (complete)

--- metodo_hill/test.py
+++ metodo_hill/test.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class FormState:
+class Decrypt:
     form_data = {}
     result = []
 
@@ -67,7 +67,6 @@ class FormState:
             print(alphabet_dict)
             alphabet_dict["_"] = 26
         elif len(alphabet) == 28:
-            print("Ingreso aqui")
             alphabet_dict = {chr(65+i): i for i in range(14)}
             alphabet_dict["Ñ"] = 14
             alphabet_dict.update({chr(65+i-1): i for i in range(15, 27)})
@@ -121,5 +120,103 @@ form_data_example = {
     "message": "A_FJKLÑOJA_N"
 }
 
-form_state = FormState()
+form_state = Decrypt()
 form_state.handle_submit(form_data_example)
+
+
+class Encrypt(Decrypt):
+    form_data = {}
+    result = []
+    
+
+    def handle_submit(self,form_data:dict):
+        alphabet =  form_data['alphabet']
+        key = form_data['matrix_key']
+        message = form_data['message']
+        message_list = [char for char in message]
+        
+        list_e=key.split(',')
+        n_matrix = abs(len(list_e) / 3)
+        
+        self.split_matrix(list_e,n_matrix)
+        
+        
+        message_numbers = []
+
+
+        # Organize by rows
+        matrix_key = self.split_matrix(list_e,n_matrix)
+
+        
+        while True:
+            if len(message_list) % n_matrix != 0:   
+                message_list.append('_')
+                continue
+            else:
+                break
+            
+            
+        alphabet_dict = {}
+
+        if len(alphabet) == 27:
+            alphabet_dict = {chr(65+i): i for i in range(26)}
+            #chr(65+i): La función chr() toma un número entero (código ASCII) y devuelve su carácter correspondiente. 65 es el código ASCII de la letra 'A'.
+            # Entonces, chr(65+i) generará los caracteres ASCII consecutivos
+            # empezando desde 'A'. Por ejemplo, cuando i es 0, chr(65+0) es 'A'. Cuando i es 1, chr(65+1) es 'B', y así sucesivamente hasta 'Z'.
+            print(alphabet_dict)
+            alphabet_dict["_"] = 26
+        elif len(alphabet) == 28:
+            print("Ingreso aqui")
+            alphabet_dict = {chr(65+i): i for i in range(14)}
+            alphabet_dict["Ñ"] = 14
+            alphabet_dict.update({chr(65+i-1): i for i in range(15, 27)})
+            alphabet_dict["_"] = 27    
+        
+        
+        message_list = [char.upper() for char in message_list]
+        for char in message_list:
+            if char in alphabet_dict:
+                message_numbers.append(alphabet_dict[char])            
+        
+        
+        matrix_finish = self.split_matrix(message_numbers, n_matrix)
+        matrix_finish = np.transpose(matrix_finish).tolist()        
+        
+        
+        # with this code solution problem --->    ValueError: data type must provide an itemsize
+        matrix_key = np.array(matrix_key, dtype=int)
+        matrix_finish = np.array(matrix_finish, dtype=int)
+        
+        multiply = np.dot(matrix_key,matrix_finish).tolist()
+        
+        if len(alphabet) == 27:
+            for i in range(len(multiply)):
+                for j in range(len(multiply[i])):
+                    multiply[i][j] = multiply[i][j] % 27
+        elif len(alphabet) == 28:
+            for i in range(len(multiply)):
+                for j in range(len(multiply[i])):
+                    multiply[i][j] = multiply[i][j] % 28
+        multiply_end =np.transpose(multiply).tolist()
+        
+        reversed_alphabet_dict = {k:v for v, k in alphabet_dict.items()}
+        letter_encrypt= []
+                
+        for i in range(len(multiply_end)):
+            for j in range(len(multiply_end[i])):
+                num = multiply_end[i][j]
+                if num in reversed_alphabet_dict:
+                    letter_encrypt.append(reversed_alphabet_dict[num])
+
+        self.result.append(letter_encrypt)
+        print("Result: ",self.result)
+
+
+form_data_example = {
+    "alphabet": "ABCDEFGHIJKLMN1OPQRSTUVWXYZ_",
+    "matrix_key": "4,3,1,2,2,1,1,1,1",
+    "message": "HOLA_MUNDO"
+}
+
+encrypt = Encrypt()
+encrypt.handle_submit(form_data_example)
